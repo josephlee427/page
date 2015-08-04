6f0026b7a64daab25b774c6f4e5b288466160ba0
josephlee427 <josephl@live.ca>
2015-08-04 10:32:58 +0900

    commented out

--- dpnm/updateTest.js
+++ dpnm/updateTest.js
@@ -1,3 +1,4 @@
+/**
 var opts, serverInfo;
 
 var cursor = Tasks.find({});
@@ -6,10 +7,10 @@ cursor.forEach(function (info) {
   console.log("info ^^")
   console.log(info.ip)
   console.log(info.port) */
-  opts = { range: [String(info.ip)], ports: String(parseInt(info.port) + 1) }
+//  opts = { range: [String(info.ip)], ports: String(parseInt(info.port) + 1) }
 //      console.log(opts)
 //      console.log("opts is above this")
-
+/**
   libnmap.nmap('scan', opts, Meteor.bindEnvironment(function(err, report){
     if (err) throw err
     report.forEach(function(item) {
@@ -17,6 +18,7 @@ cursor.forEach(function (info) {
 //        console.log(info.status)
 //        console.log(info.service)
 
+/**
     Tasks.update({ip: info.ip}, {
       $set:
       {
@@ -29,5 +31,9 @@ cursor.forEach(function (info) {
 }));
 });
 
+*/
+
+/**
 141.223.82.74
 110
+*/
