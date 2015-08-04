68b7373065736e53d1245d9ce0184ee23dbd7521
josephlee427 <josephl@live.ca>
2015-08-04 15:40:07 +0900

    minor updates

--- dpnm/dpnm.js
+++ dpnm/dpnm.js
@@ -55,10 +55,6 @@ if (Meteor.isClient) {
   });
 
   Template.task.events({
-    "click .toggle-checked": function () {
-      // Set the checked property to the opposite of its current value
-      Meteor.call("setChecked", this._id, ! this.checked);
-    },
     // Delete server info from database with a click
     "click .delete": function () {
       Meteor.call("deleteTask", this._id);
